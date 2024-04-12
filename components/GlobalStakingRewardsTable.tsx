208ecbe87fa801eedcdcfa53dcdef49c3c5ca611
Ionel <ionel2988@gmail.com>
2024-04-12 11:30:36 -0400

    update staking reward page

--- components/GlobalStakingRewardsTable.tsx
+++ components/GlobalStakingRewardsTable.tsx
@@ -1,5 +1,13 @@
-import React, { useState } from "react";
-import { Box, Link, Pagination, TableBody, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Collapse,
+  IconButton,
+  Link,
+  Pagination,
+  TableBody,
+  Typography,
+} from "@mui/material";
 import {
   TablePrimary,
   TablePrimaryCell,
@@ -8,46 +16,175 @@ import {
   TablePrimaryHeadCell,
   TablePrimaryRow,
 } from "./styled";
-import { convertTimestampToDateTime } from "../utils";
+import { convertTimestampToDateTime, shortenHex } from "../utils";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import { Tr } from "react-super-responsive-table";
+import api from "../services/api";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
-const GlobalStakingRewardsRow = ({ row }) => {
+const DetailRow = ({ row }) => {
   if (!row) {
-    return <TablePrimaryRow></TablePrimaryRow>;
+    return <TablePrimaryRow />;
   }
 
   return (
-    <TablePrimaryRow>
-      <TablePrimaryCell>
-        <Link
-          color="inherit"
-          fontSize="inherit"
-          href={`https://arbiscan.io/tx/${row.TxHash}`}
-          target="__blank"
-        >
-          {convertTimestampToDateTime(row.TimeStamp)}
-        </Link>
+    <TablePrimaryRow sx={{ borderBottom: 0 }}>
+      <TablePrimaryCell align="left">
+        {convertTimestampToDateTime(
+          row.StakeActionTimeStamp + row.TimeStampDiff
+        )}
+      </TablePrimaryCell>
+      <TablePrimaryCell align="left">
+        {convertTimestampToDateTime(
+          row.UnstakeEligibleTimeStamp + row.TimeStampDiff
+        )}
       </TablePrimaryCell>
       <TablePrimaryCell align="center">
         <Link
-          href={`/user/${row.StakerAddr}`}
-          style={{
-            color: "inherit",
-            fontSize: "inherit",
-            fontFamily: "monospace",
-          }}
+          href={`/detail/${row.TokenId}`}
+          style={{ color: "inherit", fontSize: "inherit" }}
         >
-          {row.StakerAddr}
+          {row.TokenId}
         </Link>
       </TablePrimaryCell>
+      <TablePrimaryCell align="center">{row.StakeActionId}</TablePrimaryCell>
       <TablePrimaryCell align="center">
-        {row.FullyClaimed ? "Yes" : "No"}
+        {row.Claimed ? "Yes" : "No"}
       </TablePrimaryCell>
       <TablePrimaryCell align="right">
-        {row.YourAmountToClaimEth.toFixed(6)} ETH
+        {row.AmountEth.toFixed(6)}
       </TablePrimaryCell>
     </TablePrimaryRow>
+  );
+};
+
+const DetailTable = ({ list }) => {
+  return (
+    <TablePrimaryContainer>
+      <TablePrimary>
+        <TablePrimaryHead>
+          <Tr>
+            <TablePrimaryHeadCell align="left" sx={{ py: 1 }}>
+              Stake Datetime
+            </TablePrimaryHeadCell>
+            <TablePrimaryHeadCell align="left" sx={{ py: 1 }}>
+              Unstake Datetime
+            </TablePrimaryHeadCell>
+            <TablePrimaryHeadCell sx={{ py: 1 }}>Token Id</TablePrimaryHeadCell>
+            <TablePrimaryHeadCell sx={{ py: 1 }}>
+              Action Id
+            </TablePrimaryHeadCell>
+            <TablePrimaryHeadCell sx={{ py: 1 }}>Claimed?</TablePrimaryHeadCell>
+            <TablePrimaryHeadCell align="right" sx={{ py: 1 }}>
+              Reward
+            </TablePrimaryHeadCell>
+          </Tr>
+        </TablePrimaryHead>
+        <TableBody>
+          {list.map((row, index) => (
+            <DetailRow row={row} key={index} />
+          ))}
+        </TableBody>
+      </TablePrimary>
+    </TablePrimaryContainer>
+  );
+};
+
+const GlobalStakingRewardsRow = ({ row }) => {
+  const [open, setOpen] = useState(false);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await api.get_action_ids_by_deposit_id(
+        row.StakerAddr,
+        row.DepositId
+      );
+      setList(response);
+    };
+    fetchData();
+  }, []);
+
+  if (!row) {
+    return <TablePrimaryRow />;
+  }
+
+  return (
+    <>
+      <TablePrimaryRow sx={{ borderBottom: 0 }}>
+        <TablePrimaryCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TablePrimaryCell>
+        <TablePrimaryCell>
+          <Link
+            color="inherit"
+            fontSize="inherit"
+            href={`https://arbiscan.io/tx/${row.TxHash}`}
+            target="__blank"
+          >
+            {convertTimestampToDateTime(row.TimeStamp)}
+          </Link>
+        </TablePrimaryCell>
+        <TablePrimaryCell align="center">
+          <Link
+            href={`/user/${row.StakerAddr}`}
+            style={{
+              color: "inherit",
+              fontSize: "inherit",
+              fontFamily: "monospace",
+            }}
+          >
+            {shortenHex(row.StakerAddr)}
+          </Link>
+        </TablePrimaryCell>
+        <TablePrimaryCell align="center">
+          <Link
+            href={`/prize/${row.RoundNum}`}
+            style={{
+              color: "inherit",
+              fontSize: "inherit",
+            }}
+          >
+            {row.RoundNum}
+          </Link>
+        </TablePrimaryCell>
+        <TablePrimaryCell align="center">{row.NumStakedNFTs}</TablePrimaryCell>
+        <TablePrimaryCell align="center">
+          {row.TotalDepositAmountEth.toFixed(6)}
+        </TablePrimaryCell>
+        <TablePrimaryCell align="center">
+          {row.YourTokensStaked}
+        </TablePrimaryCell>
+        <TablePrimaryCell align="center">
+          {row.FullyClaimed ? "Yes" : "No"}
+        </TablePrimaryCell>
+        <TablePrimaryCell align="right">
+          {row.YourCollectedAmountEth.toFixed(6)}
+        </TablePrimaryCell>
+        <TablePrimaryCell align="right">
+          {row.YourAmountToClaimEth.toFixed(6)}
+        </TablePrimaryCell>
+      </TablePrimaryRow>
+      <TablePrimaryRow sx={{ borderTop: 0 }}>
+        <TablePrimaryCell sx={{ py: 0 }} colSpan={10}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1, marginBottom: 4 }}>
+              <Typography variant="subtitle1" gutterBottom component="div">
+                Detail
+              </Typography>
+              <DetailTable list={list} />
+            </Box>
+          </Collapse>
+        </TablePrimaryCell>
+      </TablePrimaryRow>
+    </>
   );
 };
 
@@ -63,11 +200,21 @@ export const GlobalStakingRewardsTable = ({ list }) => {
         <TablePrimary>
           <TablePrimaryHead>
             <Tr>
-              <TablePrimaryHeadCell align="left">
+              <TablePrimaryHeadCell />
+              <TablePrimaryHeadCell align="left" sx={{ minWidth: "165px" }}>
                 Stake Datetime
               </TablePrimaryHeadCell>
               <TablePrimaryHeadCell>Staker</TablePrimaryHeadCell>
-              <TablePrimaryHeadCell>Reward Claimed?</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell>Round</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell sx={{ minWidth: "120px" }}>
+                Total Staked Tokens
+              </TablePrimaryHeadCell>
+              <TablePrimaryHeadCell>Total Deposited</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell>Staked Tokens</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell>Fully Claimed?</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell align="right">
+                Collected Amount
+              </TablePrimaryHeadCell>
               <TablePrimaryHeadCell align="right">
                 Amount to Claim
               </TablePrimaryHeadCell>
