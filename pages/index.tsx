ac50214a5f3decc0c78751e5969858a5b5d7528c
Ionel <ionel2988@gmail.com>
2024-08-07 16:57:02 -0400

    update special prize on front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -947,57 +947,108 @@ const NewHome = () => {
               <Typography variant="subtitle1" color="primary" mt={4} mb={2}>
                 Special Prizes
               </Typography>
-              <Grid container spacing={2} mb={2} alignItems="center">
-                <Grid item xs={12} sm={4} md={4}>
-                  <Typography>Endurance Champion</Typography>
-                </Grid>
-                <Grid item xs={12} sm={8} md={8}>
-                  <Typography>
-                    <Link
-                      href={`/user/${specialWinners?.EnduranceChampionAddress}`}
-                      color="rgb(255, 255, 255)"
-                      fontSize="inherit"
-                      sx={{ wordBreak: "break-all" }}
-                    >
-                      {specialWinners?.EnduranceChampionAddress}
-                    </Link>
-                    {specialWinners?.EnduranceChampionDuration > 0 && (
-                      <>
-                        {` (Lasted ${formatSeconds(
-                          specialWinners?.EnduranceChampionDuration
-                        )})`}
-                      </>
-                    )}
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid container spacing={2} mb={2} alignItems="center">
-                <Grid item xs={12} sm={4} md={4}>
-                  <Typography>Stellar Spender Address</Typography>
-                </Grid>
-                <Grid item xs={12} sm={8} md={8}>
-                  <Typography>
-                    <Link
-                      href={`/user/${specialWinners?.StellarSpenderAddress}`}
-                      color="rgb(255, 255, 255)"
-                      fontSize="inherit"
-                      sx={{ wordBreak: "break-all" }}
-                    >
-                      {specialWinners?.StellarSpenderAddress}
-                    </Link>
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid container spacing={2} mb={2} alignItems="center">
-                <Grid item xs={12} sm={4} md={4}>
-                  <Typography>Stellar Spender Amount</Typography>
-                </Grid>
-                <Grid item xs={12} sm={8} md={8}>
-                  <Typography>
-                    {specialWinners?.StellarSpenderAmountEth.toFixed(4)} ETH
-                  </Typography>
-                </Grid>
-              </Grid>
+              {data?.CurRoundNum > 0 && data?.TsRoundStart === 0 ? (
+                <>
+                  <Grid container spacing={2} mb={2} alignItems="center">
+                    <Grid item xs={12} sm={4} md={4}>
+                      <Typography>Endurance Champion</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={8}>
+                      <Typography>
+                        <Link
+                          href={`/user/${prizeInfo?.EnduranceWinnerAddr}`}
+                          color="rgb(255, 255, 255)"
+                          fontSize="inherit"
+                          sx={{ wordBreak: "break-all" }}
+                        >
+                          {prizeInfo?.EnduranceWinnerAddr}
+                        </Link>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={2} mb={2} alignItems="center">
+                    <Grid item xs={12} sm={4} md={4}>
+                      <Typography>Stellar Spender Address</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={8}>
+                      <Typography>
+                        <Link
+                          href={`/user/${prizeInfo?.StellarWinnerAddr}`}
+                          color="rgb(255, 255, 255)"
+                          fontSize="inherit"
+                          sx={{ wordBreak: "break-all" }}
+                        >
+                          {prizeInfo?.StellarWinnerAddr}
+                        </Link>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={2} mb={2} alignItems="center">
+                    <Grid item xs={12} sm={4} md={4}>
+                      <Typography>Stellar Spender Amount</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={8}>
+                      <Typography>
+                        {prizeInfo?.StellarERC20AmountEth.toFixed(4)} ETH
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </>
+              ) : (
+                <>
+                  <Grid container spacing={2} mb={2} alignItems="center">
+                    <Grid item xs={12} sm={4} md={4}>
+                      <Typography>Endurance Champion</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={8}>
+                      <Typography>
+                        <Link
+                          href={`/user/${specialWinners?.EnduranceChampionAddress}`}
+                          color="rgb(255, 255, 255)"
+                          fontSize="inherit"
+                          sx={{ wordBreak: "break-all" }}
+                        >
+                          {specialWinners?.EnduranceChampionAddress}
+                        </Link>
+                        {specialWinners?.EnduranceChampionDuration > 0 && (
+                          <>
+                            {` (Lasted ${formatSeconds(
+                              specialWinners?.EnduranceChampionDuration
+                            )})`}
+                          </>
+                        )}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={2} mb={2} alignItems="center">
+                    <Grid item xs={12} sm={4} md={4}>
+                      <Typography>Stellar Spender Address</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={8}>
+                      <Typography>
+                        <Link
+                          href={`/user/${specialWinners?.StellarSpenderAddress}`}
+                          color="rgb(255, 255, 255)"
+                          fontSize="inherit"
+                          sx={{ wordBreak: "break-all" }}
+                        >
+                          {specialWinners?.StellarSpenderAddress}
+                        </Link>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={2} mb={2} alignItems="center">
+                    <Grid item xs={12} sm={4} md={4}>
+                      <Typography>Stellar Spender Amount</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={8}>
+                      <Typography>
+                        {specialWinners?.StellarSpenderAmountEth.toFixed(4)} ETH
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </>
+              )}
             </Grid>
           )}
         </Grid>
