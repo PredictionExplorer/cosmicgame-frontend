4cd57760bec2ad86ad9407d19c0cb4cad4a40dc4
Ionel <ionel2988@gmail.com>
2024-10-11 08:29:09 -0400

    fix issue

--- hooks/useStakingWalletRWLKContract.ts
+++ hooks/useStakingWalletRWLKContract.ts
@@ -2,6 +2,6 @@ import { STAKING_WALLET_RWLK_ADDRESS } from "../config/app";
 import STAKING_WALLET_RWLK_ABI from "../contracts/StakingWalletRWalk.json";
 import useContract from "./useContract";
 
-export default function useStakingWalletCSTContract() {
+export default function useStakingWalletRWLKContract() {
   return useContract(STAKING_WALLET_RWLK_ADDRESS, STAKING_WALLET_RWLK_ABI);
 }
