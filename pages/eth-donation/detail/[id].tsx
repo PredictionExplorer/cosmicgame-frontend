8adcc2291bb9aa7118ac89d9bdc399f464eae92f
Ionel <ionel2988@gmail.com>
2025-04-01 10:17:05 -0400

    eth donation detial page

--- pages/eth-donation/detail/[id].tsx
+++ pages/eth-donation/detail/[id].tsx
@@ -9,27 +9,49 @@ import {
 } from "../../../utils";
 import api from "../../../services/api";
 
-const EthDonationDetail = ({ id }) => {
+interface EthDonationDetailProps {
+  id: number;
+}
+
+const EthDonationDetail = ({ id }: EthDonationDetailProps) => {
   const [loading, setLoading] = useState(true);
-  const [donationInfo, setDonationInfo] = useState(null);
-  const [dataJson, setDataJson] = useState(null);
-  const [metaData, setMetaData] = useState(null);
+  const [donationInfo, setDonationInfo] = useState<any>(null);
+  const [dataJson, setDataJson] = useState<any>(null);
+  const [metaData, setMetaData] = useState<any>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDonationDetail = async () => {
       setLoading(true);
-      const info = await api.get_donations_with_info_by_id(id);
-      setDonationInfo(info);
-      if (info?.DataJson) {
-        const jsonData = JSON.parse(info.DataJson);
-        setDataJson(jsonData);
-        const meta = await getMetadata(jsonData.url);
-        setMetaData(meta);
+      try {
+        // Fetch donation detail by ID
+        const info = await api.get_donations_with_info_by_id(id);
+        setDonationInfo(info);
+
+        if (info?.DataJson) {
+          const jsonData = JSON.parse(info.DataJson);
+          setDataJson(jsonData);
+
+          // Fetch metadata from provided URL in the donation info
+          const meta = await getMetadata(jsonData.url);
+          setMetaData(meta);
+        }
+      } catch (error) {
+        console.error("Failed to fetch donation details:", error);
+      } finally {
+        setLoading(false);
       }
-      setLoading(false);
     };
-    fetchData();
-  }, []);
+
+    fetchDonationDetail();
+  }, [id]);
+
+  if (loading) {
+    return (
+      <MainWrapper>
+        <Typography variant="h6">Loading...</Typography>
+      </MainWrapper>
+    );
+  }
 
   return (
     <MainWrapper>
@@ -42,103 +64,89 @@ const EthDonationDetail = ({ id }) => {
       >
         Direct (ETH) Donation Detail
       </Typography>
-      {loading ? (
-        <Typography variant="h6">Loading...</Typography>
-      ) : (
+
+      {/* Donation Date and Transaction Link */}
+      <Box mb={1} display="flex" flexWrap="wrap">
+        <Typography color="primary">Donate Datetime:</Typography>&nbsp;
+        <Link
+          href={`https://arbiscan.io/tx/${donationInfo.TxHash}`}
+          target="_blank"
+          color="inherit"
+        >
+          <Typography>
+            {convertTimestampToDateTime(donationInfo.TimeStamp)}
+          </Typography>
+        </Link>
+      </Box>
+
+      {/* Donor Address */}
+      <Box mb={1} display="flex" flexWrap="wrap">
+        <Typography color="primary">Donor Address:</Typography>&nbsp;
+        <Link href={`/user/${donationInfo.DonorAddr}`} color="inherit">
+          <Typography fontFamily="monospace">
+            {donationInfo.DonorAddr}
+          </Typography>
+        </Link>
+      </Box>
+
+      {/* Round Number */}
+      <Box mb={1} display="flex" flexWrap="wrap">
+        <Typography color="primary">Round Number:</Typography>&nbsp;
+        <Link href={`/prize/${donationInfo.RoundNum}`} color="inherit">
+          <Typography>{donationInfo.RoundNum}</Typography>
+        </Link>
+      </Box>
+
+      {/* Donation Amount */}
+      <Box mb={1} display="flex" flexWrap="wrap">
+        <Typography color="primary">Amount:</Typography>&nbsp;
+        <Typography>{donationInfo.AmountEth.toFixed(2)} ETH</Typography>
+      </Box>
+
+      {/* Additional JSON Data (if available) */}
+      {dataJson && (
         <>
           <Box mb={1} display="flex" flexWrap="wrap">
-            <Typography color="primary">Donate Datetime:</Typography>
-            &nbsp;
-            <Link
-              href={`https://arbiscan.io/tx/${donationInfo.TxHash}`}
-              style={{ color: "inherit" }}
-              target="_blank"
-            >
-              <Typography>
-                {convertTimestampToDateTime(donationInfo.TimeStamp)}
-              </Typography>
+            <Typography color="primary">Title:</Typography>&nbsp;
+            <Typography>{dataJson.title}</Typography>
+          </Box>
+
+          <Box mb={1} display="flex" flexWrap="wrap">
+            <Typography color="primary">Message:</Typography>&nbsp;
+            <Typography>{dataJson.message}</Typography>
+          </Box>
+
+          <Box mb={1} display="flex" flexWrap="wrap">
+            <Typography color="primary">URL:</Typography>&nbsp;
+            <Link href={dataJson.url} target="_blank" color="inherit">
+              {dataJson.url}
             </Link>
           </Box>
-          <Box mb={1} display="flex" flexWrap="wrap">
-            <Typography color="primary">Donor Address:</Typography>
-            &nbsp;
-            <Link
-              href={`/user/${donationInfo.DonorAddr}`}
-              style={{ color: "rgb(255, 255, 255)" }}
-            >
-              <Typography fontFamily="monospace">
-                {donationInfo.DonorAddr}
-              </Typography>
-            </Link>
-          </Box>
-          <Box mb={1} display="flex" flexWrap="wrap">
-            <Typography color="primary">Round Number:</Typography>
-            &nbsp;
-            <Link
-              href={`/prize/${donationInfo.RoundNum}`}
-              sx={{
-                fontSize: "inherit",
-                color: "inherit",
-              }}
-            >
-              <Typography>{donationInfo.RoundNum}</Typography>
-            </Link>
-          </Box>
-          <Box mb={1} display="flex" flexWrap="wrap">
-            <Typography color="primary">Amount:</Typography>
-            &nbsp;
-            <Typography>{donationInfo.AmountEth.toFixed(2)} ETH</Typography>
-          </Box>
-          {dataJson && (
-            <>
-              <Box mb={1} display="flex" flexWrap="wrap">
-                <Typography color="primary">Title:</Typography>
-                &nbsp;
-                <Typography>{dataJson?.title}</Typography>
-              </Box>
-              <Box mb={1} display="flex" flexWrap="wrap">
-                <Typography color="primary">Message:</Typography>
-                &nbsp;
-                <Typography>{dataJson?.message}</Typography>
-              </Box>
-              <Box mb={1} display="flex" flexWrap="wrap">
-                <Typography color="primary">URL:</Typography>
-                &nbsp;
-                <Typography>
-                  <Link
-                    sx={{ color: "inherit", fontSize: "inherit" }}
-                    href={dataJson?.url}
-                    target="_blank"
-                  >
-                    {dataJson?.url}
-                  </Link>
-                </Typography>
-              </Box>
-            </>
-          )}
-          {!!metaData?.description && (
-            <Box mb={1} display="flex" flexWrap="wrap">
-              <Typography color="primary">Meta Description:</Typography>
-              &nbsp;
-              <Typography>{metaData?.description}</Typography>
-            </Box>
-          )}
-          {!!metaData?.Keywords && (
-            <Box mb={1} display="flex" flexWrap="wrap">
-              <Typography color="primary">Meta Keywords:</Typography>
-              &nbsp;
-              <Typography>{metaData?.Keywords}</Typography>
-            </Box>
-          )}
-          {!!metaData?.image && (
-            <Box>
-              <Typography color="primary" mb={1}>
-                Meta Image:
-              </Typography>
-              <img src={metaData?.image} width="100%" alt="meta image" />
-            </Box>
-          )}
         </>
+      )}
+
+      {/* Meta Data (if available) */}
+      {metaData?.description && (
+        <Box mb={1} display="flex" flexWrap="wrap">
+          <Typography color="primary">Meta Description:</Typography>&nbsp;
+          <Typography>{metaData.description}</Typography>
+        </Box>
+      )}
+
+      {metaData?.Keywords && (
+        <Box mb={1} display="flex" flexWrap="wrap">
+          <Typography color="primary">Meta Keywords:</Typography>&nbsp;
+          <Typography>{metaData.Keywords}</Typography>
+        </Box>
+      )}
+
+      {metaData?.image && (
+        <Box>
+          <Typography color="primary" mb={1}>
+            Meta Image:
+          </Typography>
+          <img src={metaData.image} width="100%" alt="Meta image" />
+        </Box>
       )}
     </MainWrapper>
   );
@@ -147,8 +155,7 @@ const EthDonationDetail = ({ id }) => {
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
-  const params = context.params!.id;
-  const id = Array.isArray(params) ? params[0] : params;
+  const { id } = context.params!;
   const title = "Direct (ETH) Donation Detail | Cosmic Signature";
   const description = "Direct (ETH) Donation Detail";
 
