1020738310a025242c6f18805a066ccca1a07f9b
Ionel <ionel2988@gmail.com>
2025-04-29 10:39:06 -0400

    update nft related components

--- components/NFT.tsx
+++ components/NFT.tsx
@@ -4,24 +4,36 @@ import { formatId, getAssetsUrl } from "../utils";
 import { NFTSkeleton, NFTInfoWrapper, StyledCard } from "./styled";
 import NFTImage from "./NFTImage";
 
-const NFT = ({ nft }) => {
+// NFT component that displays an individual NFT with its image, ID, and name
+const NFT = ({
+  nft,
+}: {
+  nft: { TokenId: string; Seed: string; TokenName: string };
+}) => {
+  // Generate the URL for the NFT's image based on the 'Seed' field
   const image = getAssetsUrl(`cosmicsignature/0x${nft.Seed}.png`);
 
   return (
     <StyledCard>
       <CardActionArea>
+        {/* Show a skeleton loader if the 'nft' data is not yet available */}
         {!nft ? (
           <NFTSkeleton animation="wave" variant="rectangular" />
         ) : (
+          // Display the NFT image as a clickable link, leading to the NFT detail page
           <Link href={`/detail/${nft.TokenId}`} sx={{ display: "block" }}>
             <NFTImage src={image} />
           </Link>
         )}
+
+        {/* Display the NFT Token ID if available */}
         {nft && (
           <NFTInfoWrapper>
             <Typography variant="caption">{formatId(nft.TokenId)}</Typography>
           </NFTInfoWrapper>
         )}
+
+        {/* Display the NFT Token Name if it's not an empty string */}
         {nft.TokenName !== "" && (
           <NFTInfoWrapper sx={{ width: "calc(100% - 40px)" }}>
             <Typography
--- components/NFTVideo.tsx
+++ components/NFTVideo.tsx
@@ -3,26 +3,38 @@ import Image from "next/image";
 import { VideoCard, NFTImageWrapper } from "./styled";
 import NFTImage from "./NFTImage";
 
-const NFTVideo = ({ image_thumb, onClick }) => (
+// NFTVideo component that displays a thumbnail image with a play button overlay
+const NFTVideo = ({
+  image_thumb, // Thumbnail image URL for the NFT video
+  onClick, // Click handler for the play button
+}: {
+  image_thumb: string;
+  onClick: () => void;
+}) => (
   <VideoCard>
+    {/* Wrapper for the NFT image */}
     <NFTImageWrapper>
+      {/* Displaying the NFT image with reduced opacity */}
       <NFTImage src={image_thumb} style={{ opacity: 0.55 }} />
     </NFTImageWrapper>
+
+    {/* Play button overlay positioned at the center */}
     <div
       style={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        cursor: "pointer",
+        position: "absolute", // Positioning the button in the center
+        top: "50%", // Vertically center the button
+        left: "50%", // Horizontally center the button
+        transform: "translate(-50%, -50%)", // Adjust to truly center
+        cursor: "pointer", // Change cursor to pointer on hover
       }}
     >
+      {/* Play button icon */}
       <Image
-        src={"/images/play.svg"}
-        alt="play"
-        onClick={onClick}
-        width={85}
-        height={84}
+        src={"/images/play.svg"} // Path to the play icon
+        alt="play" // Alt text for accessibility
+        onClick={onClick} // Trigger the onClick handler when clicked
+        width={85} // Set the width of the play icon
+        height={84} // Set the height of the play icon
       />
     </div>
   </VideoCard>
