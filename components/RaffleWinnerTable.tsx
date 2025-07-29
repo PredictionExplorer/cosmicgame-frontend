ae922acd4acd1fe259ad4e3d4c8b1e5fd4c22c43
Ionel <ionel2988@gmail.com>
2025-07-29 13:12:10 -0400

    update raffle winner table

--- components/RaffleWinnerTable.tsx
+++ components/RaffleWinnerTable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { TableBody, Link, Typography, Tooltip } from "@mui/material";
+import { TableBody, Link, Typography, Tooltip, Button } from "@mui/material";
 import {
   TablePrimaryContainer,
   TablePrimaryCell,
@@ -131,7 +131,11 @@ const WinnerRow = ({ winner }) => {
           " "
         )}
       </TablePrimaryCell>
-      <TablePrimaryCell></TablePrimaryCell>
+      <TablePrimaryCell>
+        {Amount && roundTimeoutTimesToWithdrawPrizes * 1000 < Date.now() && (
+          <Button size="small">Claim</Button>
+        )}
+      </TablePrimaryCell>
     </TablePrimaryRow>
   );
 };
