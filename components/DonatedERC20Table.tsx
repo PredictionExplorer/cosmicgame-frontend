ac0f0768a7413045447fd96a48dd09c36da66792
Ionel <ionel2988@gmail.com>
2025-08-05 09:59:32 -0400

    update donated erc20 tokens table

--- components/DonatedERC20Table.tsx
+++ components/DonatedERC20Table.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, FC } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Button, Link, TableBody, Tooltip, Typography } from "@mui/material";
 import {
   TablePrimary,
@@ -14,45 +13,12 @@ import {
   formatSeconds,
   shortenHex,
 } from "../utils";
-import NFTImage from "./NFTImage";
 import { Tr } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import { CustomPagination } from "./CustomPagination";
 import useRaffleWalletContract from "../hooks/useRaffleWalletContract";
 
-// ----------------------------
-// Type Definitions
-// ----------------------------
-interface TokenRecord {
-  RecordId: string;
-  TxHash: string;
-  TimeStamp: number;
-  DonorAddr: string;
-  RoundNum: number;
-  TokenAddr: string;
-  TokenId?: string;
-  NFTTokenId?: string;
-  NFTTokenURI?: string;
-  WinnerAddr?: string;
-  Index: number;
-}
-
-interface TokenRowProps {
-  token: TokenRecord;
-  handleClaim?: (tokenIndex: number) => void;
-}
-
-interface DonatedNFTTableProps {
-  list: TokenRecord[];
-  handleClaim?: (tokenIndex: number) => void;
-}
-
-// ----------------------------
-// NFT Row Component
-// ----------------------------
-const TokenRow: FC<TokenRowProps> = ({ token, handleClaim }) => {
-  const [tokenURI, setTokenURI] = useState<any>(null);
-
+const TokenRow = ({ token, handleClaim }) => {
   const [
     roundTimeoutTimesToWithdrawPrizes,
     setRoundTimeoutTimesToWithdrawPrizes,
@@ -73,21 +39,6 @@ const TokenRow: FC<TokenRowProps> = ({ token, handleClaim }) => {
       fetchRoundTimeoutTimesToWithdrawPrizes();
     }
   }, [raffleWalletContract]);
-
-  useEffect(() => {
-    const fetchTokenMetadata = async () => {
-      try {
-        const { data } = await axios.get(token.NFTTokenURI!);
-        setTokenURI(data);
-      } catch (error) {
-        console.error("Error fetching token URI:", error);
-      }
-    };
-
-    if (token.NFTTokenURI) {
-      fetchTokenMetadata();
-    }
-  }, [token.NFTTokenURI]);
 
   if (!token) return <TablePrimaryRow />;
 
@@ -150,20 +101,31 @@ const TokenRow: FC<TokenRowProps> = ({ token, handleClaim }) => {
         </Tooltip>
       </TablePrimaryCell>
 
-      {/* Token ID */}
       <TablePrimaryCell align="center">
-        {tokenURI?.external_url ? (
-          <Link
-            href={tokenURI.external_url}
-            target="_blank"
-            sx={{ fontSize: "inherit", color: "inherit" }}
-          >
-            {token.NFTTokenId || token.TokenId}
-          </Link>
-        ) : (
-          token.NFTTokenId || token.TokenId
-        )}
+        {token.AmountEth.toFixed(2)}
       </TablePrimaryCell>
+
+      {/* Winner Address */}
+      <TablePrimaryCell>
+        <Tooltip title={token.WinnerAddr}>
+          <Link
+            href={`/user/${token.WinnerAddr}`}
+            style={{
+              color: "inherit",
+              fontSize: "inherit",
+              fontFamily: "monospace",
+            }}
+            target="_blank"
+          >
+            {shortenHex(token.WinnerAddr, 6)}
+          </Link>
+        </Tooltip>
+      </TablePrimaryCell>
+
+      <TablePrimaryCell align="center">
+        {token.Claimed ? "Yes" : "No"}
+      </TablePrimaryCell>
+
       <TablePrimaryCell align="center">
         {convertTimestampToDateTime(roundTimeoutTimesToWithdrawPrizes)}{" "}
         {roundTimeoutTimesToWithdrawPrizes < Date.now() / 1000
@@ -171,17 +133,6 @@ const TokenRow: FC<TokenRowProps> = ({ token, handleClaim }) => {
           : `(${formatSeconds(
               roundTimeoutTimesToWithdrawPrizes - Math.ceil(Date.now() / 1000)
             )})`}
-      </TablePrimaryCell>
-
-      {/* Token Image */}
-      <TablePrimaryCell sx={{ width: "130px" }}>
-        {tokenURI?.image ? (
-          <Link href={tokenURI.external_url} target="_blank">
-            <NFTImage src={tokenURI.image} />
-          </Link>
-        ) : (
-          <Typography variant="body2">Image not available</Typography>
-        )}
       </TablePrimaryCell>
 
       {/* Claim Button */}
@@ -205,7 +156,7 @@ const TokenRow: FC<TokenRowProps> = ({ token, handleClaim }) => {
 // ----------------------------
 // Main Table Component
 // ----------------------------
-const DonatedERC20Table: FC<DonatedNFTTableProps> = ({ list, handleClaim }) => {
+const DonatedERC20Table = ({ list, handleClaim }) => {
   const perPage = 5;
   const [page, setPage] = useState<number>(1);
 
@@ -227,9 +178,10 @@ const DonatedERC20Table: FC<DonatedNFTTableProps> = ({ list, handleClaim }) => {
               <TablePrimaryHeadCell align="left">
                 Token Address
               </TablePrimaryHeadCell>
-              <TablePrimaryHeadCell>Token ID</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell>Amount</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell>Winner</TablePrimaryHeadCell>
+              <TablePrimaryHeadCell>Claimed</TablePrimaryHeadCell>
               <TablePrimaryHeadCell>Expiration Date</TablePrimaryHeadCell>
-              <TablePrimaryHeadCell>Token Image</TablePrimaryHeadCell>
               {handleClaim && <TablePrimaryHeadCell></TablePrimaryHeadCell>}
             </Tr>
           </TablePrimaryHead>
