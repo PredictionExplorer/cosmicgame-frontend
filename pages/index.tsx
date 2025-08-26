aa61d75849bda9ed13905719d2b124dc7bd90961
Ionel <ionel2988@gmail.com>
2025-08-26 13:25:10 -0400

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -145,7 +145,7 @@ const NewHome = () => {
 
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("md"));
-  console.log("ASDF");
+  console.log("12321ASDF");
   interface TabPanelProps {
     children?: React.ReactNode;
     index: number;
