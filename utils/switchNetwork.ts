b10a8d4244dff762332dd1fdb45583b51fe938b8
Ionel <ionel2988@gmail.com>
2025-08-26 12:35:34 -0400

    switch network to sepolia

--- utils/switchNetwork.ts
+++ utils/switchNetwork.ts
@@ -1,63 +1,63 @@
 const accountRequest = (ethereum: any) => {
-  return ethereum.request({ method: 'eth_requestAccounts' })
-}
+  return ethereum.request({ method: "eth_requestAccounts" });
+};
 
 const switchRequest = (ethereum: any) => {
   return ethereum.request({
-    method: 'wallet_switchEthereumChain',
-    params: [{ chainId: '0x7A69' }],  // local testnet
-    // params: [{ chainId: '0x66eee' }],  // sepolia
-  })
-}
+    method: "wallet_switchEthereumChain",
+    // params: [{ chainId: "0x7A69" }], // local testnet
+    params: [{ chainId: "0x66eee" }], // sepolia
+  });
+};
 
 const addChainRequest = (ethereum: any) => {
-  return ethereum.request({
-    method: 'wallet_addEthereumChain',
-    params: [
-      {
-        chainId: '0x7A69',
-        chainName: 'Localhost 22945',
-        rpcUrls: ['http://161.129.67.42:22945'],
-        nativeCurrency: {
-          name: 'AGOR',
-          symbol: 'AGOR',
-          decimals: 18,
-        },
-      },
-    ],
-  })
   // return ethereum.request({
   //   method: 'wallet_addEthereumChain',
   //   params: [
   //     {
-  //       chainId: '0x66eee',
-  //       chainName: 'Arbitrum Sepolia',
-  //       rpcUrls: ['https://sepolia-rollup.arbitrum.io/rpc'],
+  //       chainId: '0x7A69',
+  //       chainName: 'Localhost 22945',
+  //       rpcUrls: ['http://161.129.67.42:22945'],
   //       nativeCurrency: {
-  //         name: 'ETH',
-  //         symbol: 'ETH',
+  //         name: 'AGOR',
+  //         symbol: 'AGOR',
   //         decimals: 18,
   //       },
   //     },
   //   ],
   // })
-}
+  return ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [
+      {
+        chainId: "0x66eee",
+        chainName: "Arbitrum Sepolia",
+        rpcUrls: ["https://sepolia-rollup.arbitrum.io/rpc"],
+        nativeCurrency: {
+          name: "ETH",
+          symbol: "ETH",
+          decimals: 18,
+        },
+      },
+    ],
+  });
+};
 
 export const switchNetwork = async () => {
-  const { ethereum } = window
+  const { ethereum } = window;
   if (ethereum) {
     try {
-      await accountRequest(ethereum)
-      await switchRequest(ethereum)
+      await accountRequest(ethereum);
+      await switchRequest(ethereum);
     } catch (error) {
       if (error.code === 4902) {
         try {
-          await addChainRequest(ethereum)
-          await switchRequest(ethereum)
+          await addChainRequest(ethereum);
+          await switchRequest(ethereum);
         } catch (addError) {
-          console.log(addError)
+          console.log(addError);
         }
       }
     }
   }
-}
+};
